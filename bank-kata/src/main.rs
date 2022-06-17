b0ae63cec50fdfbca917cd42fe40dac5289d33d5
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-06-17 17:34:18 +0200

    add bank kata description

---
+++ bank-kata/src/main.rs
@@ -0,0 +1,47 @@
+/// Problem description
+///
+/// Create a simple bank application with the following features:
+/// - deposit into account
+/// - withdraw from account
+/// - print a bank statement to the console
+///
+/// **Acceptance criteria**
+///
+/// Statement should have transactons in the following format:
+///
+/// ```
+/// > DATE       | AMOUNT  | BALANCE
+/// > 10/04/2014 | 500.00  | 1400.00
+/// > 02/04/2014 | -100.00 | 900.00
+/// > 01/04/2014 | 1000.00 | 1000.00
+/// ```
+///
+/// Contraints
+///
+/// 1. Start with the following structure
+///
+/// ```
+/// struct Account;
+///
+/// impl Account {
+///     pub fn deposit(amount: i32) -> () {
+///         todo!()
+///     }
+///
+///     pub fn withdraw(amount: i32) -> () {
+///         todo!()
+///     }
+///
+///     pub fn print_statement() -> () {
+///         todo!()
+///     }
+/// }
+/// ```
+///
+/// 2. You are not allowed to add any other public method to this (except for `new`)
+/// 3. Use Strings and Integers for date and amounts (KISS)
+/// 4. Don't worry about spacing in the printed statement (optionally format)
+
+fn main() {
+    println!("Hello, world!");
+}
